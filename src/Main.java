b10a0cbf4aa2086c199c29dd94a33c1d8a08d198
invassiion <invassiion@gmail.com>
2024-05-27 09:10:29 +0700

    arrays

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import kataPackage.KataCountingSheep;
 import kataPackage.KataDoubleArraySum;
 import kataPackage.KataLessThenN;
+import kataPackage.Solution;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,20 +11,41 @@ public class Main {
         System.out.println("____________________________");
 
         System.out.println("KataDoubleArraySum");
-        double[] numbers ={2.3, 3.4, 45.6};
+        double[] numbers = {2.3, 3.4, 45.6};
         System.out.println(KataDoubleArraySum.sum(numbers));
 
         System.out.println("____________________________");
         System.out.println("KataCountingSheep");
 
-        Boolean[] array = {true,  true,  true,  false,
-                true,  true,  true,  true ,
-                true,  false, true,  false,
-                true,  false, false, true ,
-                true,  true,  true,  true ,
-                false, false, true,  true};
+        Boolean[] array = {true, true, true, false,
+                true, true, true, true,
+                true, false, true, false,
+                true, false, false, true,
+                true, true, true, true,
+                false, false, true, true};
 
         System.out.println(KataCountingSheep.countSheeps(array));
         System.out.println("____________________________");
+
+
+        Solution merger = new Solution();
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        merger.merge(nums1, m, nums2, n);
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\n ____________________________");
+
+        Solution remover = new Solution();
+        int[] nums = {1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5};
+        int k = remover.removeDuplicates(nums);
+        System.out.println("Количество уникальных элементов (не более двух раз): " + k);
+        System.out.print("Массив после удаления дубликатов: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
