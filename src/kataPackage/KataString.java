751cdaf040c356b90614928faeaa868adff9304c
invassiion <invassiion@yandex.ru>
2024-09-10 12:50:25 +0700

    WhoLikesIt, cakes;

--- src/kataPackage/KataString.java
+++ src/kataPackage/KataString.java
@@ -60,4 +60,36 @@ public class KataString {
             return "Yes";
         } else return "No";
     }
+
+    public static  String whoLikesIt(String... names){
+         if (names.length == 0) {
+             return "no one likes this";
+         } else if (names.length == 1) {
+             return  names[0] + " likes this";
+         } else if (names.length == 2) {
+             return names[0] + "and " + names[1] + " like this";
+         } else if (names.length == 3) {
+             return names[0] + ", " + names[1] + "and " +  names[2] + " like this";
+         } else if (names.length >= 4 ){
+             return names[0] +", " + names[1] + " and " + (names.length -2) +" others like this";
+         }
+
+        return "";
+    }
+    public static int cakes(Map<String, Integer> recipe, Map<String, Integer> available){
+      int maxCakes = Integer.MAX_VALUE;
+
+      for (Map.Entry<String, Integer> ingredient : recipe.entrySet()) {
+          String ingredientName = ingredient.getKey();
+          int requiredAmount = ingredient.getValue();
+
+          if (available.containsKey(ingredientName)) {
+              int availableAmount = available.get(ingredientName);
+              maxCakes = Math.min(maxCakes, availableAmount / requiredAmount);
+          } else {
+              return 0;
+          }
+      }
+      return maxCakes;
+    }
 }
